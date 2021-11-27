2869fe32571d3b46b351412ebdb8677663d13fe2
Rakibul Hasan⚡ <xenax.rakibul@gmail.com>
2021-11-27 07:55:04 +0600

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import RichTextEditor from "components/Editor";
 import Layout from "components/layout/Layout";
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>Rise of Kingdom - Lost Toolkit</title>
+      </Head>
       <RichTextEditor />
     </Layout>
   );
